2e7ecbc42669bba32eb4c34dd1d43ab83d1ea5c7
Aaron Rosenberg <aarongrosenberg@gmail.com>
2012-03-17 13:33:22 -0400

    added some specs to test the gem

--- spec/unique_permutation_spec.rb
+++ spec/unique_permutation_spec.rb
@@ -2,6 +2,29 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe Array do
   describe '#unique_permutation' do
-    it 'generates unique permuations'
+    before(:all) do
+      @array = [1, 2, 2]
+    end
+
+    it 'should take a block that gets each array permutation' do
+      @array.unique_permutation {|p| p.should be_a(Array) }
+    end
+
+    it 'should return an enum if no block is given' do
+      @array.unique_permutation.should be_a(Enumerator)
+    end
+
+    it 'should not have duplicate permutations' do
+      permutations = @array.unique_permutation.to_a
+      permutations.select {|p| p == permutations.first }.count.should be 1
+    end
+
+    it 'generates only unique permutations in arrays with repeated elements' do
+      @array.unique_permutation.to_a.count.should be 3
+    end
+
+    it 'should return fewer elements than the built in Ruby permutation method making it faster' do
+      @array.unique_permutation.count.should be < @array.permutation.count
+    end
   end
 end
